5dc37389b0cb94b504b15bc7a24dae4fe64b325b
allenkays <allenkays@tutanota.com>
2023-04-17 15:50:13 -0400

    Change square sides to be equal

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
 
-class square():
+class Square():
+    """
+    A Class representing a square
+    """
     
-    width = 0
-
-    
-    def __init__(self, *args, **kwargs):
+    def __init__(self, **kwargs):
+        """
+        Initializes a square object with given width and height
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
+        """
+        Area of the square
+        """
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.width * 2)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.width)
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
